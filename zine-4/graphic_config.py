d2daabe3127974e9c28099dbb7db681ac916ff43
Paula Friedrich <pauliebe@users.noreply.github.com>
2017-09-18 13:43:50 -0500

    Adding PF graphics 170918

---
+++ zine-4/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1R6il-hXrjWGs3OF0IMC1yUUQvUPk33y3Jw7vqFuc3eI'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
